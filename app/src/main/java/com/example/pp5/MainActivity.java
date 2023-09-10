d4b573c94a915b259449a85d56172b844b16cca4
mcieslinski <maciekkv@interia.pl>
2023-09-10 15:13:38 +0200

    Navigation drawer with fragments

--- app/src/main/java/com/example/pp5/MainActivity.java
+++ app/src/main/java/com/example/pp5/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.pp5;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
@@ -9,35 +11,55 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.example.pp5.fragments.FavouriteFragment;
 import com.example.pp5.fragments.HomeFragment;
 import com.example.pp5.fragments.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNav;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
         bottomNav = findViewById(R.id.bottomNavigationView);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new HomeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new HomeFragment()).commit();
     }
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment selectedFragment = null;
 
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
                 case R.id.home:
-                   selectedFragment = new HomeFragment();
-                   break;
+                    selectedFragment = new HomeFragment();
+                    break;
                 case R.id.search:
                     selectedFragment = new SearchFragment();
                     break;
@@ -45,10 +67,40 @@ public class MainActivity extends AppCompatActivity {
                     selectedFragment = new FavouriteFragment();
                     break;
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,selectedFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, selectedFragment).commit();
 
             return true;
         }
     };
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_about:
+                Toast.makeText(MainActivity.this,"About app",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_stats:
+                Toast.makeText(MainActivity.this,"Statistic",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_sync:
+                Toast.makeText(MainActivity.this,"Sync up to date",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_button:
+                Toast.makeText(MainActivity.this,"Enable button",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_main:
+                Toast.makeText(MainActivity.this,"Main Home",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+        return true;
+    }
 }
+
